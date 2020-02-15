09cfcd03484d7246d9a26af3a529617d5b65c77d
Markus Paasovaara <markus.paasovaara@barona.fi>
2020-02-15 12:16:38 +0200

    create a list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Creating a random list!");
+    /*
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+    */
+
+    let input = read_input();
+    print_output(input);
+}
+
+fn read_input() -> Vec<String> {
+    println!("Reading input variables");
+    let vec: Vec<String> = vec![
+        "Markus".to_string(),
+        "Batman".to_string(),
+        "Joker".to_string(),
+        "Alfred".to_string()
+    ];
+    vec
+}
+
+fn print_output(output: Vec<String>) {
+    for out in &output {
+        println!("{}", out);
+    }
 }
