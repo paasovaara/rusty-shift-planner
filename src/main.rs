766eab43ce08c58d139fdf0e11841baa124c1c38
Markus Paasovaara <markus.paasovaara@barona.fi>
2020-02-16 09:38:56 +0200

    Read names from file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate rand;
 
 //use std::io;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 use std::{thread, time::Duration};
 use rand::thread_rng;
 use rand::seq::SliceRandom;
@@ -18,7 +21,8 @@ fn main() {
     println!("You guessed: {}", guess);
     */
 
-    let mut input = read_input();
+    let mut input = read_file_content("names.txt".to_string());
+
     let mut random = thread_rng();
     input.shuffle(&mut random);
 //let print_closure = |name: &String| {
@@ -51,4 +55,16 @@ fn print_output(output: Vec<String>/*, printer: &dyn Fn(&String) -> &String*/) {
         //
         // println!("{}", printer(out));
     }
+}
+
+fn read_file_content(path: String) -> Vec<String> {
+    let f = File::open(path).expect("Unable to open file");
+    let reader = BufReader::new(f);
+    let mut lines: Vec<String> = Vec::new();
+    for line in reader.lines() {
+        let line_content = line.expect("Unable to read line");
+        //println!("Line: {}", line_content);
+        lines.push(line_content.trim().to_string());
+    }
+    lines
 }
